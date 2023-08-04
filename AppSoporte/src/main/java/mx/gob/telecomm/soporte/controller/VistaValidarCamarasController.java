61ba9e5abac905efc8f617cd21687b4e8d81ec11
luu92 <luu.vale.05@gmail.com>
2023-08-04 17:12:47 -0600

    Se agrego apartado de validar Camaras

--- AppSoporte/src/main/java/mx/gob/telecomm/soporte/controller/VistaValidarCamarasController.java
+++ AppSoporte/src/main/java/mx/gob/telecomm/soporte/controller/VistaValidarCamarasController.java
@@ -1,5 +1,19 @@
 package mx.gob.telecomm.soporte.controller;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import mx.gob.telecomm.soporte.application.App;
+
 public class VistaValidarCamarasController {
+
+	@FXML
+	void regresarPrincipal() {
+		try {
+			App.setRoot("views/admin/ReporteSoporteTecnicoVistaAdmin");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
